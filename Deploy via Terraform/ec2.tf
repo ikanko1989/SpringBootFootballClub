88c1d7aaae38428f0fc18a39102332ebb62b8f43
ikanko1989 <139227790+ikanko1989@users.noreply.github.com>
2025-07-25 12:46:03 +0200

    Create ec2.tf

---
+++ Deploy via Terraform/ec2.tf
@@ -0,0 +1,12 @@
+resource "aws_instance" "springboot_app" {
+  ami           = var.ami_id
+  instance_type = var.instance_type
+  key_name      = aws_key_pair.deployer.key_name
+  security_groups = [aws_security_group.allow_ssh_ping_8080.name]
+
+  user_data = file("${path.module}/user_data.conf")
+
+  tags = {
+    Name = "SpringBootApp"
+  }
+}
