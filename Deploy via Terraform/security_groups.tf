111bb68f68f4bb3fd0fa53043f44596bbfa1c9f0
ikanko1989 <139227790+ikanko1989@users.noreply.github.com>
2025-07-25 12:45:11 +0200

    Create security_groups.tf

---
+++ Deploy via Terraform/security_groups.tf
@@ -0,0 +1,36 @@
+resource "aws_security_group" "allow_ssh_ping_8080" {
+  name        = "allow_ssh_ping_8080"
+  description = "Allow SSH, ICMP (ping), and TCP 8080"
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = -1
+    to_port     = -1
+    protocol    = "icmp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 8080
+    to_port     = 8080
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "allow-ssh-ping-8080"
+  }
+}
