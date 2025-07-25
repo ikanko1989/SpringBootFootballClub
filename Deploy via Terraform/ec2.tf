0820ceaa0f6a081eabb8ff0df27f711d1889bc90
ikanko1989 <139227790+ikanko1989@users.noreply.github.com>
2025-07-25 13:40:53 +0200

    Update ec2.tf

--- Deploy via Terraform/ec2.tf
+++ Deploy via Terraform/ec2.tf
@@ -1,7 +1,7 @@
 resource "aws_instance" "springboot_app" {
   ami           = var.ami_id
   instance_type = var.instance_type
-  key_name      = aws_key_pair.deployer.key_name
+  key_name      = var.key_pair_name
   security_groups = [aws_security_group.allow_ssh_ping_8080.name]
 
   user_data = file("${path.module}/user_data.conf")
